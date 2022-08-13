82c8befbba32e39d664e6e97d04dd54bb0f06904
Tetsuaki Hamano / 浜野 哲明 <tetsuaki.hamano@gmail.com>
2022-08-13 11:25:46 +0000

    コード修正

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,12 +1,57 @@
 /**
  * External dependencies
  */
+import { TextlintKernel } from '@textlint/kernel';
 import CodeMirror from '@uiw/react-codemirror';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data';
+
+const textlintKernel = new TextlintKernel();
+
+const options = {
+	ext: '.md',
+	plugins: [
+		{
+			pluginId: 'markdown',
+			plugin: require( 'textlint-plugin-markdown' ),
+		},
+	],
+};
+
+const code = `## Title
+
+\`\`\`jsx
+function Demo() {
+  return <div>demo</div>
+}
+\`\`\`
+
+\`\`\`bash
+# Not dependent on uiw.
+npm install @codemirror/lang-markdown --save
+npm install @codemirror/language-data --save
+\`\`\`
+
+[weisit ulr](https://uiwjs.github.io/react-codemirror/)
+
+\`\`\`go
+package main
+import "fmt"
+func main() {
+  fmt.Println("Hello, 世界")
+}
+\`\`\`
+`;
 
 const CodeEditor = () => {
+	textlintKernel.lintText( 'TODO: text', options ).then( () => {} );
+
 	return (
 		<div className="code-editor">
-			<CodeMirror value="Test" />
+			<CodeMirror
+				value={ code }
+				extensions={ [ markdown( { base: markdownLanguage, codeLanguages: languages } ) ] }
+			/>
 		</div>
 	);
 };
