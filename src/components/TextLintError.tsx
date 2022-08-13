6d90b718acfa4250f464707b7736da7370a208c2
Tetsuaki Hamano / 浜野 哲明 <tetsuaki.hamano@gmail.com>
2022-08-13 15:30:28 +0000

    エラーレイアウト調整

--- src/components/TextLintError.tsx
+++ src/components/TextLintError.tsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 /**
  * External dependencies
  */
-import { Stack, Alert, Card, Typography } from '@mui/material';
+import { Stack, Alert, Card } from '@mui/material';
 
 /**
  * internal dependencies
@@ -14,19 +14,19 @@ const TextLintError = () => {
 	const { lintError } = useContext( TextLintContext );
 
 	return (
-		<>
+		<Stack spacing={ 2 }>
 			{ lintError.length > 0 ? (
-				<Stack spacing={ 2 }>
-					{ lintError.map( ( error: any, index: number ) => (
-						<Card key={ index }>
-							<Alert severity="error">{ error.message }</Alert>
-						</Card>
-					) ) }
-				</Stack>
+				lintError.map( ( error: any, index: number ) => (
+					<Card key={ index }>
+						<Alert severity="error">{ error.message }</Alert>
+					</Card>
+				) )
 			) : (
-				<Typography>エラーはありません。</Typography>
+				<Card>
+					<Alert severity="success">エラーはありません。</Alert>
+				</Card>
 			) }
-		</>
+		</Stack>
 	);
 };
 
