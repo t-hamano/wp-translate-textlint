22f25300647d099d2893de0f4debe629ea54d623
Tetsuaki Hamano / 浜野 哲明 <tetsuaki.hamano@gmail.com>
2022-08-14 15:03:15 +0000

    textlintルールの追加・調整

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,15 @@ import type { ThemeOptions } from '@mui/material/styles';
 
 export const githubRepoUrl = 'https://github.com/t-hamano/wp-translate-textlint' as const;
 
+// @ts-ignore
+import pluginMarkdown from 'textlint-plugin-markdown';
+// @ts-ignore
+import noTodo from 'textlint-rule-no-todo';
+// @ts-ignore
+import maxKanjiContinuousLen from 'textlint-rule-max-kanji-continuous-len';
+// @ts-ignore
+import noMixedZenkakuAndHankakuAlphabet from 'textlint-rule-no-mixed-zenkaku-and-hankaku-alphabet';
+
 export const themeOptions: ThemeOptions = {
 	typography: {
 		fontFamily: '"Open Sans", sans-serif',
@@ -34,29 +43,30 @@ export const textlintOptions = {
 	plugins: [
 		{
 			pluginId: 'markdown',
-			plugin: require( 'textlint-plugin-markdown' ),
+			plugin: pluginMarkdown,
 		},
 	],
 	rules: [
+		// TODOという文字列が入っているかをチェックする
+		// See: https://github.com/textlint-rule/textlint-rule-no-todo
 		{
 			ruleId: 'no-todo',
-			rule: require( 'textlint-rule-no-todo' ).default,
+			rule: noTodo,
 		},
-		{
-			ruleId: 'no-todo',
-			rule: require( 'textlint-rule-no-todo' ).default,
-		},
-		{
-			ruleId: 'ja-space-between-half-and-full-width',
-			rule: require( 'textlint-rule-ja-space-between-half-and-full-width' ),
-		},
+		// 漢字が連続する最大文字数を制限する (最大5文字まで)
+		// See: https://github.com/textlint-ja/textlint-rule-max-kanji-continuous-len
 		{
 			ruleId: 'max-kanji-continuous-len',
-			rule: require( 'textlint-rule-max-kanji-continuous-len' ),
+			rule: maxKanjiContinuousLen,
+			options: {
+				max: 5,
+			},
 		},
+		// 全角アルファベットをチェックする
+		// See: https://github.com/textlint-ja/textlint-rule-no-mixed-zenkaku-and-hankaku-alphabet
 		{
 			ruleId: 'no-mixed-zenkaku-and-hankaku-alphabet',
-			rule: require( 'textlint-rule-no-mixed-zenkaku-and-hankaku-alphabet' ),
+			rule: noMixedZenkakuAndHankakuAlphabet,
 		},
 	],
 };
